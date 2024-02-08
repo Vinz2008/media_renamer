f1c677528325d715c74f8ecb8b05174127496954
Vinz2008 <vincentbidarddelanoe@gmail.com>
2024-02-08 15:42:56 +0100

    add tests for pattern removing

--- src/tests.rs
+++ src/tests.rs
@@ -1,17 +1,32 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn one_number_test() {
+    fn season_append_one_number() {
         let filename = "series - 1 - [BDrip] - (2022)".to_string();
         
         let renamed_filename = crate::rename_file(&filename, 1);
-        assert!(renamed_filename == "series - S01E01 - [BDrip] - (2022)")
+        assert!(renamed_filename == "series - S01E01 - [BDrip] - (2022)");
     }
     #[test]
-    fn two_number_test() {
+    fn season_append_two_number(){
         let filename = "series - 10 - [BDrip] - (2022)".to_string();
         
         let renamed_filename = crate::rename_file(&filename, 1);
-        assert!(renamed_filename == "series - S01E10 - [BDrip] - (2022)")
+        assert!(renamed_filename == "series - S01E10 - [BDrip] - (2022)");
     }
+    #[test]
+    fn pattern_remove(){
+        let filename = "series - 10 - [BDrip] - (2022)".to_string();
+        let pattern = "BDrip".to_string();
+        let renamed_filename = crate::remove_pattern(&filename, &pattern);
+        assert!(renamed_filename == "series - 10 - [] - (2022)");
+    }
+    #[test]
+    fn pattern_remove2(){
+        let filename = "series.x264.HP.AC3-2.0.(2022)".to_string();
+        let pattern = "x264.HP.AC3-2.0.".to_string();
+        let renamed_filename = crate::remove_pattern(&filename, &pattern);
+        assert!(renamed_filename == "series.(2022)");
+    }
+
 }
